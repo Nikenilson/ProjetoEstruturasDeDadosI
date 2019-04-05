3f5399722b76baafa84c4179609888cf26d2ff37
KaiFlame <42344215+KaiFlame@users.noreply.github.com>
2019-04-05 13:44:16 -0300

    4r

--- MatrizEsparsa/MatrizEsparsa/Form1.Designer.cs
+++ MatrizEsparsa/MatrizEsparsa/Form1.Designer.cs
@@ -40,10 +40,16 @@
             this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -104,6 +110,11 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.numericUpDown3);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.numericUpDown2);
             this.groupBox1.Controls.Add(this.label7);
@@ -112,14 +123,14 @@
             this.groupBox1.Font = new System.Drawing.Font("Gill Sans MT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(15, 60);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(277, 146);
+            this.groupBox1.Size = new System.Drawing.Size(278, 459);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Gerar Matriz";
+            this.groupBox1.Text = "Métodos";
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(96, 41);
+            this.numericUpDown1.Location = new System.Drawing.Point(107, 41);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(49, 32);
             this.numericUpDown1.TabIndex = 7;
@@ -127,31 +138,31 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 43);
+            this.label6.Location = new System.Drawing.Point(17, 43);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(84, 30);
+            this.label6.Size = new System.Drawing.Size(89, 30);
             this.label6.TabIndex = 7;
-            this.label6.Text = "Colunas";
+            this.label6.Text = "Colunas:";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(6, 81);
+            this.label7.Location = new System.Drawing.Point(17, 81);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(67, 30);
+            this.label7.Size = new System.Drawing.Size(72, 30);
             this.label7.TabIndex = 8;
-            this.label7.Text = "Linhas";
+            this.label7.Text = "Linhas:";
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(96, 79);
+            this.numericUpDown2.Location = new System.Drawing.Point(107, 79);
             this.numericUpDown2.Name = "numericUpDown2";
             this.numericUpDown2.Size = new System.Drawing.Size(49, 32);
             this.numericUpDown2.TabIndex = 9;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(169, 53);
+            this.button1.Location = new System.Drawing.Point(173, 50);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(90, 52);
             this.button1.TabIndex = 7;
@@ -165,6 +176,51 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(466, 288);
             this.dataGridView1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Gill Sans MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(35, 198);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 71);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Pegar valor";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(87, 155);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(108, 30);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Valorzinho";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Gill Sans MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(160, 198);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 71);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Remover valor";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(163, 299);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(49, 32);
+            this.numericUpDown3.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(17, 301);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(144, 30);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Somar a todos:";
             // 
             // Form1
             // 
@@ -188,6 +244,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -207,6 +264,11 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
     }
 }
 
