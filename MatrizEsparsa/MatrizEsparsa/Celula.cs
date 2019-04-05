fade0b2f8dce7501bb0f9e92a733fe895d4a6cf5
KaiFlame <42344215+KaiFlame@users.noreply.github.com>
2019-04-05 09:12:04 -0300

    dssdds

---
+++ MatrizEsparsa/MatrizEsparsa/Celula.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrizEsparsa
+{
+    class Celula
+    {
+        Celula direita, abaixo;
+        int linha, coluna;
+        double valor;
+
+        public int Linha { get => linha; set => linha = value; }
+        public int Coluna { get => coluna; set => coluna = value; }
+        public double Valor { get => valor; set => valor = value; }
+        internal Celula Direita { get => direita; set => direita = value; }
+        internal Celula Abaixo { get => abaixo; set => abaixo = value; }
+    }
+}
