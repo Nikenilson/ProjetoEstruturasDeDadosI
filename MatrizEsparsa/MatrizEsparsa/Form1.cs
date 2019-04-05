25f55cb42087d3ad52bfdd90e1b82d4751256f70
KaiFlame <42344215+KaiFlame@users.noreply.github.com>
2019-04-05 08:44:46 -0300

    rer

---
+++ MatrizEsparsa/MatrizEsparsa/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatrizEsparsa
+{
+    public partial class Form1 : Form
+    {
+
+        int X = 0;
+        int Y = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            /*this.MouseDown += new MouseEventHandler(Form1_MouseDown);
+            this.MouseMove += new MouseEventHandler(Form1_MouseMove);*/
+        }
+
+        /*private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+            X = this.Left - MousePosition.X;
+            Y = this.Top - MousePosition.Y;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+            this.Left = X + MousePosition.X;
+            this.Top = Y + MousePosition.Y;
+        }*/
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
